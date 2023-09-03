c6e9dc8ff360dddbcdf103d581cc9cf5a82c61f2
Balaji Sundararaman <bala.srm@gmail.com>
2023-09-03 22:16:59 +0530

    create a new python script

---
+++ brand_new_file.py
@@ -0,0 +1,9 @@
+# this is a test python script
+import pandas as  pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# load the tips dataset from seaborn
+df = sns.load_datasets('tips')
+df.head()
